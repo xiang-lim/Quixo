93f10c1cdf8f73a21f615983ca134ee3902b4aa6
xiang <lim.xiang1010@gmail.com>
2025-10-06 17:29:52 +0800

    Add initial project files.

---
+++ quixo.py
@@ -0,0 +1,33 @@
+import random
+import math
+
+def check_move(board, turn, index, push_from):
+    # implement your function here
+    return True
+
+def apply_move(board, turn, index, push_from):
+    # implement your function here
+    return board[:]
+
+def check_victory(board, who_played):
+    # implement your function here
+    return -1
+
+def computer_move(board, turn, level):
+    # implement your function here
+    return (0,'B')
+    
+def display_board(board):
+    # implement your function here
+    pass
+
+def menu():
+    # implement your function here
+    pass
+
+ 
+if __name__ == "__main__":
+    menu()
+
+
+    
