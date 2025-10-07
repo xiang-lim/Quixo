a071ba6057db2643eb82074898d1cc76ecbf00b5
Jowen768 <jowenkuahworkspace@gmail.com>
2025-10-07 11:42:17 +0800

    Add Check Move

--- quixo.py
+++ quixo.py
@@ -3,8 +3,51 @@ import math
 
 
 def check_move(board, turn, index, push_from):
+def check_move(board, turn, index, push_from):
     # implement your function here
-    return True
+    n = int(math.sqrt(len(board)))
+    # [1st row, 1st col, last row, last col]
+    perimeter_index = [j for j in range(n)] + \
+                        [i * n for i in range(n)] + \
+                            [(n - 1) * n + j for j in range(n)] + \
+                                [i * n + (n - 1) for i in range(n)]
+    perimeter_index = sorted(set(perimeter_index))
+
+    # not an index for a cube in the perimeter OR the cube is not mine
+    if index not in perimeter_index or board[index] not in [turn, 0]: 
+        return False
+    else:
+        corner_index = [0*n , n-1, (n-1)*n, (n-1)*n + (n-1)]
+        corner_moves = [["B", "R"], ["B", "L"], ["T", "R"], ["T", "L"]]
+
+        # check if the index is for a cube at the corner
+        if index in corner_index: 
+            # create a dictionary of avail moves for each corner cube
+            corner_moves_dict = {corner_index[i]:corner_moves[i] for i in range(len(corner_index))}
+            # move is not avail for that specific corner cube
+            if push_from not in corner_moves_dict[index]:
+                return False
+            else: # move is avail for that specific corner cube
+                return True
+        else: # in between 2 corners
+            # Pick 2nd element from corner index, check if index less than, If no, pick the next larger element. 
+            # Case 1: less than 2nd element >> B,L,R
+            if index < corner_index[1] and push_from in ["B", "L", "R"]:
+                return True
+            
+            # Case 2: less than 3rd element
+                    #   A: if index multiple of n means 1st col, 
+                    #       >> T, B, R
+                    #   B: else last col
+                    #       >> T, B, L
+            elif index < corner_index[2] and \
+                (n % index == 0 and push_from in ["T", "B", "R"]) or (n % index != 0 and push_from not in ["T", "B", "L"]):
+                    return True
+            # Case 3: less than 4th aka largest element >> T,L,R
+            elif index < corner_index[3] and push_from  in ["T", "L", "R"]: 
+                return True
+            else:
+                return False
 
 def apply_move(board, turn, index, push_from):
     # implement your function here
