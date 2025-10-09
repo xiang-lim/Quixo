26eab78fde993de995a7f5618e29e62727b40c60
xiang <lim.xiang1010@gmail.com>
2025-10-09 19:16:41 +0800

    format and refactor the code

--- quixo.py
+++ quixo.py
@@ -99,12 +99,12 @@ def apply_move(board, turn, index, push_from):
                     # The swapping
                     count += 1
 
-    elif push_from == "T": 
+    elif push_from == "T":
         for n in range(1, length + 1):
             if index in range(length * (n - 1), length * n):
                 count = 0
                 board_copy[index] = turn
-                while count < n - 1: 
+                while count < n - 1:
                     (
                         board_copy[index - length * count],
                         board_copy[index - length * (count + 1)],
@@ -188,28 +188,41 @@ def check_victory(board, who_played):
 
         else:
             return who_played
-def get_list_of_perimeter_index (board):
+
+
+def get_list_of_perimeter_index(board):
     # Get perimeter
     board_length = cal_board_length(board)
-    list_of_perimeter_index = list(range(board_length))+list(range(len(board)-board_length,len(board)))
-    #left perimeter is c * n right side is c*n + n -1 ignore first and last row
-    for c in range(1,board_length-1):
-        list_of_perimeter_index += [c*board_length,c*board_length+board_length-1] 
+    list_of_perimeter_index = list(range(board_length)) + list(
+        range(len(board) - board_length, len(board))
+    )
+    # left perimeter is c * n right side is c*n + n -1 ignore first and last row
+    for c in range(1, board_length - 1):
+        list_of_perimeter_index += [
+            c * board_length,
+            c * board_length + board_length - 1,
+        ]
     return list_of_perimeter_index
 
-def get_valid_push_directions (board,turn):
+
+def get_valid_push_directions(board, turn):
     list_of_board_perimeter_index = get_list_of_perimeter_index(board)
-    movable_index = [value for value in list_of_board_perimeter_index if board[value] == turn or board[value] == 0]
-    list_of_directions = ["T","B","L","R"]
+    movable_index = [
+        value
+        for value in list_of_board_perimeter_index
+        if board[value] == turn or board[value] == 0
+    ]
+    list_of_directions = ["T", "B", "L", "R"]
     list_of_valid_push_directions = []
-    #Create tuple of index and direction
+    # Create tuple of index and direction
     for index in movable_index:
         for direction in list_of_directions:
             if check_move(board, turn, index, direction):
-                list_of_valid_push_directions.append((index,direction))
+                list_of_valid_push_directions.append((index, direction))
     return list_of_valid_push_directions
 
-def separate_immediate_winning_move (board,turn,list_of_valid_player_push):
+
+def separate_immediate_winning_move(board, turn, list_of_valid_player_push):
     winning_move_list = []
     viable_move_list = []
     for valid_push in list_of_valid_player_push:
@@ -217,33 +230,49 @@ def separate_immediate_winning_move (board,turn,list_of_valid_player_push):
         winner = check_victory(hypothetical_board, turn)
         if winner == turn:
             winning_move_list.append(valid_push)
-        elif winner ==0:
+        elif winner == 0:
             viable_move_list.append((valid_push, hypothetical_board))
-    return winning_move_list,viable_move_list
+    return winning_move_list, viable_move_list
+
+
+def choose_random_index_from_list(list_of_index):
+    return list_of_index[random.randint(0, len(list_of_index) - 1)]
+
 
 def computer_move(board, turn, level):
     # implement your function here
     list_of_valid_push = get_valid_push_directions(board, turn)
     if level == 1:
-        return list_of_valid_push[random.randint(0, len(list_of_valid_push)-1)]
+        return choose_random_index_from_list(list_of_valid_push)
     elif level == 2:
-        winning_move_list, optional_move_list=separate_immediate_winning_move(board, turn, list_of_valid_push)
-        if len(winning_move_list)>0:
+        winning_move_list, optional_move_list = separate_immediate_winning_move(
+            board, turn, list_of_valid_push
+        )
+        if len(winning_move_list) > 0:
             return winning_move_list[0]
         else:
-            enemy = 2 if turn ==1 else 1
+            enemy = 2 if turn == 1 else 1
             list_of_valid_move_against_enemy = []
+            list_of_enemy_win_move = []
             for move in optional_move_list:
                 reference_hypothetical_board = move[1]
-                enemy_valid_push = get_valid_push_directions(reference_hypothetical_board, enemy)
-                enemy_wining_move, enemy_optional_move = separate_immediate_winning_move(reference_hypothetical_board,enemy,enemy_valid_push)
+                enemy_valid_push = get_valid_push_directions(
+                    reference_hypothetical_board, enemy
+                )
+                enemy_wining_move, enemy_optional_move = (
+                    separate_immediate_winning_move(
+                        reference_hypothetical_board, enemy, enemy_valid_push
+                    )
+                )
                 if len(enemy_wining_move) == 0:
                     list_of_valid_move_against_enemy.append(move[0])
+                else:
+                    list_of_enemy_win_move.append(move[0])
             if list_of_valid_move_against_enemy:
-                return list_of_valid_move_against_enemy[random.randint(0, len(list_of_valid_move_against_enemy)-1)]
+                return choose_random_index_from_list(list_of_valid_move_against_enemy)
             else:
-                return optional_move_list[random.randint(0,len(optional_move_list)-1)][0]
-            
+                return choose_random_index_from_list(list_of_enemy_win_move)
+
     return (0, "B")
 
 
@@ -288,10 +317,13 @@ def generate_options_for_display(list_of_options: list):
         display_string += "\n" + "[{0}] {1}".format(i + 1, list_of_options[i])
     return display_string + "\nEnter: "
 
-def selecting_index (board,turn):
-    valid_index= False
+
+def selecting_index(board, turn):
+    valid_index = False
     while not valid_index:
-        index = get_user_input("Player {0}, Please select the piece you would like to move: ".format(turn))
+        index = get_user_input(
+            "Player {0}, Please select the piece you would like to move: ".format(turn)
+        )
         if index not in range(len(board)):
             print("Invalid Selection: Index out of range")
             continue
@@ -348,7 +380,12 @@ def menu():
     players_detail = {}
     for player_num in range(1, 3):
         initialise_player(player_num, players_detail)
-    map_of_user_input_to_board_direction = {1: "T",  2: "B", 3: "L", 4: "R",}
+    map_of_user_input_to_board_direction = {
+        1: "T",
+        2: "B",
+        3: "L",
+        4: "R",
+    }
     list_of_push_values = list(map_of_user_input_to_board_direction.values())
     print(players_detail)
 
@@ -356,7 +393,7 @@ def menu():
     turn = 1
     has_winner = False
     while not has_winner:
-        print ("Player {0} turn\n".format(turn))
+        print("Player {0} turn\n".format(turn))
         if players_detail[turn][0] == "Human":
             # logic for human
             valid_push = False
@@ -364,14 +401,14 @@ def menu():
                 display_board(board)
                 index = selecting_index(board, turn)
                 push_from = map_of_user_input_to_board_direction[
-                        get_user_input_validate_range(
-                            list_of_push_values,
-                            "Player {0}, Please select push directions{1}".format(turn,
-                                generate_options_for_display(list_of_push_values)
-                            ),
-                            "Invalid Selection: Select push direction within range",
-                        )
-                    ]
+                    get_user_input_validate_range(
+                        list_of_push_values,
+                        "Player {0}, Please select push directions{1}".format(
+                            turn, generate_options_for_display(list_of_push_values)
+                        ),
+                        "Invalid Selection: Select push direction within range",
+                    )
+                ]
                 valid_push = check_move(board, turn, index, push_from)
                 if not valid_push:
                     print("Move entered is invalid.")
@@ -380,7 +417,7 @@ def menu():
         else:
             move = computer_move(board, turn, players_detail[turn][1])
             print("(Computer) Player {0} applied move {1}".format(turn, str(move)))
-            board= apply_move(board,turn,move[0],move[1])
+            board = apply_move(board, turn, move[0], move[1])
             display_board(board)
 
         # Check victory logic
